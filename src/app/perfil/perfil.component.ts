16d97cdfcb05a23399164ff3785a7f0494a3c858
amaaj16 <amaaj16@hotmail.com>
2018-08-13 23:15:12 -0500

    Perfiles component

--- src/app/perfil/perfil.component.ts
+++ src/app/perfil/perfil.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Perfil} from '../modelos/perfil'
+import {PerfilserviceService} from '../servicios/perfilservice.service'
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.component.html',
   styleUrls: ['./perfil.component.css']
 })
 export class PerfilComponent implements OnInit {
-
-  constructor() { }
+	perfiles: Perfil[];
+  constructor(private _perfilService: PerfilserviceService) { }
 
   ngOnInit() {
+
+  	this._perfilService.getPerfiles().subscribe(data=>{
+  		this.perfiles=data;
+  	})
+
+  }
+
+  deletePerfil(area:Area){
+  	this._perfilService.deletePerfil(area).subscribe(data=>{
+  		this.perfiles= this.perfiles.filter(u=> u != perfiles);
+  		 alert("Area Eliminada Exitosamente");
+  	})
   }
 
 }
